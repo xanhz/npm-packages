9ec4663052d898dfe2586f0545b9270ccafff35d
xanhz <Zincat0107@gmail.com>
2023-09-29 22:10:25 +0700

    Add lowerBound and upperBound

---
+++ packages/dsa/src/algorithms/search/__test__/upper-bound.spec.ts
@@ -0,0 +1,20 @@
+import { upperBound } from '../upper-bound';
+
+describe('Lower Bound', () => {
+  const numbers: number[] = [1, 3, 5, 7, 9, 10, 10, 22];
+
+  it('Should return 3', () => {
+    const index = upperBound(numbers, 5, (a, b) => a - b);
+    expect(index).toBe(3);
+  });
+
+  it('Should return 4', () => {
+    const index = upperBound(numbers, 8, (a, b) => a - b);
+    expect(index).toBe(4);
+  });
+
+  it('Should return 8', () => {
+    const index = upperBound(numbers, 22, (a, b) => a - b);
+    expect(index).toBe(8);
+  });
+});
